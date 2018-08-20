8d929258c1f08016deee41c15e61cdfb7ffb1f79
Jason Liu <jason.liu@cashrewards.com>
2018-08-20 11:00:24 +1000

    automation script for new starter

---
+++ createUserFromGam.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+firstName=$1
+lastName=$2
+displayName="$firstName $lastName"
+dotCat="$firstName.$lastName"
+lowercase=$(echo "$dotCat" | tr '[:upper:]' '[:lower:]')
+email="$lowercase@cashrewards.com"
+
+echo $email
+echo $displayName
+echo $lowercase
+
+/Users/jason.liu/bin/gam/gam create user $email firstname $firstName  lastname $lastName password "P@ssword1" changepassword off
+/Users/jason.liu/bin/gam/gam update group 2million@cashrewards.com add member user $email
+/Users/jason.liu/bin/gam/gam update group jira-users@cashrewards.com add member user $email
+/Users/jason.liu/bin/gam/gam update group slack-users@cashrewards.com add member user $email
+
+replaceText="replaceText"
+
+#echo $powerShell 
+sed -i -e "s/${replaceText}/${displayName}/g" powerShellTemplate.json
+
+#cat powerShellTemplate.json
+
+ 
+aws ssm send-command --document-name "AWS-RunPowerShellScript" --document-version "\$DEFAULT" --targets "Key=instanceids,Values=i-0007a190dbe1ad5a7" --parameters file://powerShellTemplate.json  --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --region ap-southeast-2 --profile crtool
+
+sed -i -e "s/${displayName}/${replaceText}/g" powerShellTemplate.json
---
+++ createUserOnAWSDomain.ps1
@@ -0,0 +1,16 @@
+$displayName =  "John Smith"
+$firstName = $displayName.Split(' ')[0]
+$lastName=$displayName.Split(' ')[1]
+New-ADUser -Name ($firstName +' '+ $lastName) `
+-AccountPassword (ConvertTo-SecureString -AsPlainText -Force -String "P@ssword1") `
+-SamAccountName ($firstName.ToLower() +'.'+ $lastName.ToLower()) `
+-DisplayName ($firstName + ' '+ $lastName) `
+-EmailAddress  ($firstName.ToLower() +'.'+ $lastName.ToLower()+'@cashrewards.com')`
+-Enabled $True `
+-GivenName $firstName `
+-PassThru `
+-Path "OU=Technical,OU=Users,OU=Cashrewards,DC=awsad,DC=internal,DC=cashrewards,DC=com" `
+-PasswordNeverExpires $True `
+-Surname $lastName `
+-UserPrincipalName  ($firstName.ToLower() +'.'+ $lastName.ToLower()+'@awsad.internal.cashrewards.com')
+Add-ADGroupMember -Identity CR-VPN2 -Members ($firstName.ToLower() +'.'+ $lastName.ToLower())
