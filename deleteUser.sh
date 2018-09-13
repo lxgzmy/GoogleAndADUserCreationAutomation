e9d95b1a1b4b8f2a75b23a6e6e7eca4d15cbbe7a
Jason Liu <jason.liu@cashrewards.com>
2018-09-13 14:12:20 +1000

    refactor the delete function

--- deleteUser.sh
+++ deleteUser.sh
@@ -11,16 +11,16 @@ echo $email
 echo $displayName
 echo $lowercase
 
-#/Users/jason.liu/bin/gam/gam delete user $email
+/Users/jason.liu/bin/gam/gam delete user $email
 
 replaceText="replaceText"
 
 #echo $powerShell 
-sed -i -e "s/${replaceText}/${displayName}/g" powerShellDeleteTemplate.json
+#sed -i -e "s/${replaceText}/${displayName}/g" powerShellDeleteTemplate.json
 
 #cat powerShellDeleteTemplate.json
 
-aws ssm send-command --document-name "AWS-RunPowerShellScript" --document-version "\$DEFAULT" --targets "Key=instanceids,Values=i-0007a190dbe1ad5a7" --parameters file://powerShellDeleteTemplate.json  --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --region ap-southeast-2 --profile crtool
+# aws ssm send-command --document-name "AWS-RunPowerShellScript" --document-version "\$DEFAULT" --targets "Key=instanceids,Values=i-0007a190dbe1ad5a7" --parameters file://powerShellDeleteTemplate.json  --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --region ap-southeast-2 --profile crtool
 
 sed -i -e "s/${displayName}/${replaceText}/g" powerShellDeleteTemplate.json
 
